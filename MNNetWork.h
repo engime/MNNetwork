95c2ebdf67b9c22ce3c27641868acf2aa65b5521
月杪 <moonkinstar@gmail.com>
2015-12-25 17:02:10 +0800

    update

--- MNNetWork.h
+++ MNNetWork.h
@@ -16,10 +16,4 @@
 
 + (void)GET:(NSURL * _Nonnull)url params:(NSString * _Nonnull)params completionHandler:(void (^ _Nonnull)(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error))completionHandler;
 
-+ (void)POSTWITHIMAGE: (NSString * _Nonnull)url postParems: (NSMutableDictionary * _Nonnull)postParems picFilePath: (NSString * _Nonnull)picFilePath picFileName: (NSString * _Nonnull)picFileName completionHandler:(void (^ _Nonnull)(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error))completionHandler;
-
-+ (void)POSTWITHIMAGES: (NSString * _Nonnull)url postParems: (NSMutableDictionary * _Nonnull)postParems images: (NSMutableDictionary * _Nonnull)images completionHandler:(void (^ _Nonnull)(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error))completionHandler;
-
-+ (void)POSTWITHVOICE: (NSString * _Nonnull)url completionHandler:(void (^ _Nonnull)(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error))completionHandler;
-
 @end
